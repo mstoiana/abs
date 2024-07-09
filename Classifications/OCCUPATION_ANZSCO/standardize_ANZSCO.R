75251b8dfb1f03fb9cbde12c10a83353b2f268fa
Joshua Thomas <79579427+Macbeagle@users.noreply.github.com>
2024-07-09 13:39:25 +1000

    ANZSCO Initial commit

---
+++ Classifications/OCCUPATION_ANZSCO/standardize_ANZSCO.R
@@ -0,0 +1,56 @@
+library(readxl)
+library(tidyverse)
+library(openssl)
+library(dplyr)
+#TODO
+#FIX SO THAT PREDOM SKILLS ARE SHOWN ON HIGHER LAYER
+#function to import csv
+Occupation_Data <- read_excel("C:/Users/joshu/OneDrive/Documents/GIthub/abs/Classifications/OCCUPATION_ANZSCO/Input/1220.0 anzsco version 1.3 structure v1.xlsx", sheet = "Table 5", skip = 4)
+Major_Group_Data <- read_excel("C:/Users/joshu/OneDrive/Documents/GIthub/abs/Classifications/OCCUPATION_ANZSCO/Input/1220.0 anzsco version 1.3 structure v1.xlsx", sheet = "Table 1", skip = 4)
+Sub_Major_Group_Data <- read_excel("C:/Users/joshu/OneDrive/Documents/GIthub/abs/Classifications/OCCUPATION_ANZSCO/Input/1220.0 anzsco version 1.3 structure v1.xlsx", sheet = "Table 2", skip = 4)
+Minor_Group_Data <- read_excel("C:/Users/joshu/OneDrive/Documents/GIthub/abs/Classifications/OCCUPATION_ANZSCO/Input/1220.0 anzsco version 1.3 structure v1.xlsx", sheet = "Table 3", skip = 4)
+Unit_Group_Data <- read_excel("C:/Users/joshu/OneDrive/Documents/GIthub/abs/Classifications/OCCUPATION_ANZSCO/Input/1220.0 anzsco version 1.3 structure v1.xlsx", sheet = "Table 4", skip = 4)
+colnames(Occupation_Data) <- c("Major_Group", "Sub_Major_Group", "Minor_Group","Unit_Group","Occupation_Code","Description","Skill_Level")
+colnames(Major_Group_Data) <- c("Major_Group", "Description","Predominant_Skill_Levels")
+colnames(Sub_Major_Group_Data) <- c("Major_Group", "Sub_Major_Group", "Description","Predominant_Skill_Levels")
+colnames(Minor_Group_Data) <- c("Major_Group", "Sub_Major_Group", "Minor_Group", "Description","Predominant_Skill_Levels")
+colnames(Unit_Group_Data) <- c("Major_Group", "Sub_Major_Group", "Minor_Group", "Unit_Group", "Description","Skill_Levels")
+
+#create function to go through and 
+#standardize the Data
+Major_Group_Data <- Major_Group_Data %>% select(Major_Group, Description, Predominant_Skill_Levels) %>% filter(!is.na(Major_Group) & !is.na(Description))
+Sub_Major_Group_Data <- Sub_Major_Group_Data %>% select(Sub_Major_Group, Description, Predominant_Skill_Levels) %>% filter(!is.na(Description))
+Minor_Group_Data <- Minor_Group_Data %>% select(Minor_Group, Description, Predominant_Skill_Levels) %>% filter(!is.na(Minor_Group) & !is.na(Description))
+Unit_Group_Data <- Unit_Group_Data %>% select(Unit_Group, Description, Skill_Levels) %>% filter(!is.na(Unit_Group) & !is.na(Description))
+Occupation_Data <- Occupation_Data %>% select(Occupation_Code, Description, Skill_Level) %>% filter(!is.na(Occupation_Code) & !is.na(Description) & !is.na(Skill_Level))
+
+#add a date column to all Dataframes that has current date
+Major_Group_Data$Date <- as.Date(Sys.Date())
+Sub_Major_Group_Data$Date <- as.Date(Sys.Date())
+Minor_Group_Data$Date <- as.Date(Sys.Date())
+Unit_Group_Data$Date <- as.Date(Sys.Date())
+Occupation_Data$Date <- as.Date(Sys.Date())
+
+Major_Group_Data$Major_Group_key <- openssl::md5(as.character(Major_Group_Data$Major_Group))
+Sub_Major_Group_Data$Sub_Major_Group_key <- openssl::md5(as.character(Sub_Major_Group_Data$Sub_Major_Group))
+Minor_Group_Data$Minor_Group_key <- openssl::md5(as.character(Minor_Group_Data$Minor_Group))
+Unit_Group_Data$Unit_Group_key <- openssl::md5(as.character(Unit_Group_Data$Unit_Group))
+Occupation_Data$Occupation_Code_key <- openssl::md5(as.character(Occupation_Data$Occupation_Code))
+
+Major_Group_Data <- Major_Group_Data %>% select(Major_Group_key, everything())
+Sub_Major_Group_Data <- Sub_Major_Group_Data %>% select(Sub_Major_Group_key, everything())
+Minor_Group_Data <- Minor_Group_Data %>% select(Minor_Group_key, everything())
+Unit_Group_Data <- Unit_Group_Data %>% select(Unit_Group_key, everything())
+Occupation_Data <- Occupation_Data %>% select(Occupation_Code_key, everything())
+
+#write to CSV
+
+write_csv(Major_Group_Data, "C:/Users/joshu/OneDrive/Documents/GIthub/abs/Classifications/OCCUPATION_ANZSCO/Output/ANZSCO_Major_Group.csv")
+write_csv(Sub_Major_Group_Data, "C:/Users/joshu/OneDrive/Documents/GIthub/abs/Classifications/OCCUPATION_ANZSCO/Output/ANZSCO_Sub_Major_Group.csv")
+write_csv(Minor_Group_Data, "C:/Users/joshu/OneDrive/Documents/GIthub/abs/Classifications/OCCUPATION_ANZSCO/Output/ANZSCO_Minor_Group.csv")
+write_csv(Unit_Group_Data, "C:/Users/joshu/OneDrive/Documents/GIthub/abs/Classifications/OCCUPATION_ANZSCO/Output/ANZSCO_Unit_Group.csv")
+write_csv(Occupation_Data, "C:/Users/joshu/OneDrive/Documents/GIthub/abs/Classifications/OCCUPATION_ANZSCO/Output/ANZSCO_Occupation_Code.csv")
+
+
+
+
