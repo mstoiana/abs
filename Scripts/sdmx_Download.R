291d29083704f65234e10e820c039fb2b56e3db9
Joshua Thomas <79579427+Macbeagle@users.noreply.github.com>
2024-07-16 13:42:17 +1000

    Create sdmx_Download.R

---
+++ Scripts/sdmx_Download.R
@@ -0,0 +1,20 @@
+if (!requireNamespace("rsdmx", quietly = TRUE)) {
+  install.packages("rsdmx")
+}
+
+library(rsdmx)
+
+#Set static variables
+providerId <- "ABS"
+resource <- "data"
+
+read_sdmx_data <- function(providerId = "ABS", resource = "data", flowRef = "ALC", startDate = NULL, endDate = NULL) {
+  # Construct args for rsdmx
+  args <- list(providerId = providerId, resource = resource, flowRef = flowRef)
+  
+  # Call readSDMX with the constructed argument list
+  sdmx <- do.call(readSDMX, args)
+  data_df <- as.data.frame(sdmx)
+  
+  return(data_df)
+}
