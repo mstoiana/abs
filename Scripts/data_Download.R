6cfae891a6580eb8ddbff91a7604174dfd821d16
Joshua Thomas <79579427+Macbeagle@users.noreply.github.com>
2024-07-22 09:15:41 +1000

    Update data_Download.R
    add download_census_data documentation

--- Scripts/data_Download.R
+++ Scripts/data_Download.R
@@ -31,6 +31,26 @@ download_file <- function(url, dest_path, extract_path, file_name){
   }
 }
 
+#' @param c_year The census year for which data is being requested.
+#' @param c_pack The specific data pack of interest within the census data.
+#' @param c_geo The geographical classification of the data (e.g., national, state).
+#' @param c_area The specific area for which data is required.
+#' @param dest_path The file path where the downloaded zip file should be saved.
+#' @param extract_path The directory path where the contents of the zip file should be extracted.
+#' @param metadata A boolean flag indicating whether to retain only metadata files in the extraction directory.
+#'                 If TRUE, only files containing "Metadata" in their name are retained, and all others are deleted.
+#'
+#' @details The function constructs a download URL using the provided parameters and calls an external function
+#'          `download_file` to handle the download and extraction process. After extraction, if the `metadata` parameter
+#'          is set to TRUE, the function iterates through the extracted files, removing any that do not pertain to
+#'          metadata. This is useful for users interested only in the metadata files and not in the full dataset.
+#'
+#' @return None.
+#'
+#' @examples
+#' download_census_data("2021", "G01", "NAT", "AUS", "path/to/download", "path/to/extract", TRUE)
+#' This example downloads the 2021 National Census G01 data pack for Australia, extracts it, and retains only metadata files.
+
 download_census_data <- function(c_year, c_pack, c_geo, c_area, dest_path, extract_path, metadata){
   url <- paste0("https://www.abs.gov.au/census/find-census-data/datapacks/download/", c_year, "_", c_pack, "_", c_geo, "_for_", c_area, "_short-header.zip")
   file_name <- paste0(c_year, "_", c_pack, "_", c_geo, "_for_", c_area, "_short-header.zip")
